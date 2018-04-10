815679e488dd9deba06a06c29a0ec9aee7fd74ea
Michal Szelag <szelag.michal@gmail.com>
2018-04-10 16:49:30 +0100

    Second Commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'ms-portfolio',
+    title: 'Michal Szelag Portfolio',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,7 @@
 <template>
+  
   <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        ms-portfolio
-      </h1>
-      <h2 class="subtitle">
-        Michal Szelag Portfolio
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+    <app-logo/>
   </section>
 </template>
 
